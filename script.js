c84d38fe48f1d7b8bbdb8b5fcc8a8081ced188f5
DYLMAZZ <dylanmazzanti@sheridan.edu>
2026-09-07 20:58:11 +0000

    updated script.js to handle calculations

--- script.js
+++ script.js
@@ -5,7 +5,28 @@ function processRow(e) {
     let subtotal = (qty * cost).toFixed(2);
     $(this).find('input[name="itemTotal"]').val(subtotal);
   });
+
+  processTotals(e); //doing this as a separate function
 };
+
+function processTotals(e) {
+    let subtotal = 0;
+    // sum all elements in table with name="itemTotal
+    $(e).closest('table').find('input[name="itemTotal"]').each(function(){
+        subtotal += parseFloat($(this).val()) || 0;
+    });
+    
+    $('#subTotal').val(subtotal.toFixed(2));
+
+    let taxRate = parseFloat($('#tax').val()) || 0;
+    let taxTotal = subtotal * (taxRate / 100);
+
+    $('#taxTotal').val(taxTotal.toFixed(2));
+
+    let total = subtotal + taxTotal;
+
+    $('#total').val(total.toFixed(2));
+}
 
 function addRow(e) {
   let clone = $(e).closest('table').find('tbody tr:last-child').clone(true);
